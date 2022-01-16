02d977b11e95c71d0269fcbea8e1f1609b9beaea
Marcos Paulo <marcos_pa32@hotmail.com>
2022-01-15 22:16:42 -0300

    Criação de cidade e estado, e repository destes

--- src/main/java/com/cursojpa/cursojpa/CursojpaApplication.java
+++ src/main/java/com/cursojpa/cursojpa/CursojpaApplication.java
@@ -3,8 +3,12 @@ package com.cursojpa.cursojpa;
 import java.util.Arrays;
 
 import com.cursojpa.cursojpa.domain.Categoria;
+import com.cursojpa.cursojpa.domain.Cidade;
+import com.cursojpa.cursojpa.domain.Estado;
 import com.cursojpa.cursojpa.domain.Produto;
 import com.cursojpa.cursojpa.repository.CategoriaRepository;
+import com.cursojpa.cursojpa.repository.CidadeRepository;
+import com.cursojpa.cursojpa.repository.EstadoRepository;
 import com.cursojpa.cursojpa.repository.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,10 @@ public class CursojpaApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursojpaApplication.class, args);
@@ -42,6 +50,19 @@ public class CursojpaApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 	}
 
 
---
+++ src/main/java/com/cursojpa/cursojpa/repository/CidadeRepository.java
@@ -0,0 +1,12 @@
+
+package com.cursojpa.cursojpa.repository;
+
+import com.cursojpa.cursojpa.domain.Cidade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+    
+}
