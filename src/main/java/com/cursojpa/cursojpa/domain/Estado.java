82de501d2b8f917ca8d5908ea5ef68f8091accb4
Marcos Paulo <marcos_pa32@hotmail.com>
2022-01-17 18:53:55 -0300

    Ajustes na serialização JSON

--- src/main/java/com/cursojpa/cursojpa/domain/Estado.java
+++ src/main/java/com/cursojpa/cursojpa/domain/Estado.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -23,7 +23,7 @@ public class Estado implements Serializable {
     private Integer Id;
     private String nome;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades = new ArrayList<>();
 
