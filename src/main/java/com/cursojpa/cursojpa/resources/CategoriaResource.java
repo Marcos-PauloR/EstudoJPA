c87286dfb551e1cd7b7c6bc3a572dec6cbd9dd4b
Marcos Paulo <marcos_pa32@hotmail.com>
2022-01-21 19:30:42 -0300

    Criação da paginação de Categorias

--- src/main/java/com/cursojpa/cursojpa/resources/CategoriaResource.java
+++ src/main/java/com/cursojpa/cursojpa/resources/CategoriaResource.java
@@ -9,16 +9,18 @@ import com.cursojpa.cursojpa.dto.CategoriaDTO;
 import com.cursojpa.cursojpa.service.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping(value="/categoria/")
+@RequestMapping(value="/categoria")
 public class CategoriaResource{
 
     @Autowired
@@ -59,5 +61,16 @@ public class CategoriaResource{
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @RequestMapping(value = "/page",  method=RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page, 
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction ){
+
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
+        return ResponseEntity.ok().body(listDTO);
+    }
 
 }
--- src/main/java/com/cursojpa/cursojpa/service/CategoriaService.java
+++ src/main/java/com/cursojpa/cursojpa/service/CategoriaService.java
@@ -10,6 +10,9 @@ import com.cursojpa.cursojpa.service.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -48,4 +51,8 @@ public class CategoriaService {
         return repository.findAll();
     }
 
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
+    }
 }
