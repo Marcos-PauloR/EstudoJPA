b8d1944912b32db158b9d46cc0d8755805e75da2
Marcos Paulo <marcos_pa32@hotmail.com>
2023-03-17 08:48:32 -0300

    Feat(Produtos): Criad Endpoint de busca de produtos por categoria+soma de preços

--- src/main/java/com/cursojpa/cursojpa/repository/ProdutoRepository.java
+++ src/main/java/com/cursojpa/cursojpa/repository/ProdutoRepository.java
@@ -1,9 +1,24 @@
 package com.cursojpa.cursojpa.repository;
 
+import com.cursojpa.cursojpa.domain.Categoria;
 import com.cursojpa.cursojpa.domain.Produto;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Integer>{
-    
+ 
+    @Transactional(readOnly = true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> search(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+
+    @Transactional(readOnly = true)
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+
 }
---
+++ src/main/java/com/cursojpa/cursojpa/resources/ProdutoResource.java
@@ -0,0 +1,53 @@
+package com.cursojpa.cursojpa.resources;
+
+import com.cursojpa.cursojpa.domain.Pedido;
+import com.cursojpa.cursojpa.domain.Produto;
+import com.cursojpa.cursojpa.dto.ProdutoDTO;
+import com.cursojpa.cursojpa.resources.utils.URL;
+import com.cursojpa.cursojpa.service.PedidoService;
+import com.cursojpa.cursojpa.service.ProdutoService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource{
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id){
+
+        Produto obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<ProdutoDTO>> findPage(
+            @RequestParam(value="nome",defaultValue = "") String nome,
+            @RequestParam(value="categorias",defaultValue = "") String categorias,  
+            @RequestParam(defaultValue = "0") Integer page, 
+            @RequestParam(defaultValue = "24") Integer linesPerPage, 
+            @RequestParam(defaultValue = "nome") String orderBy, 
+            @RequestParam(defaultValue = "ASC") String direction ){
+        
+        List<Integer> lista = URL.decodeIntList(categorias);
+        String nomeDecoded = URL.decodeParam(nome);   
+        Page<Produto> list = service.search(nomeDecoded, lista, page, linesPerPage, orderBy, direction);
+        Page<ProdutoDTO > listDTO = list.map(obj -> new ProdutoDTO(obj));
+        
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+}
